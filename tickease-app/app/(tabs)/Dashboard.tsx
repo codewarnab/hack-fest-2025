15773b90a5898da66669ce2d5023a86841900469
codewarnab <dp950810@gmail.com>
2025-04-06 12:35:12 +0530

    changed splash screen

---
+++ tickease-app/app/(tabs)/Dashboard.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import AnalyticsContent from '../analytics/index';
+
+export default function Dashboard() {
+    // Render the Analytics content directly within the Dashboard tab
+    return <AnalyticsContent />;
+}
